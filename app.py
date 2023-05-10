fb1ac1e4bb11dea3657818717f878c6f65f96159
sashabernthal <sasha.bernthal@menloschool.org>
2023-05-10 12:39:05 -0700

    final updates

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ app.secret_key = "123secret"
 
 @app.route("/", methods = ["GET", "POST"])
 def login():
+    user = db_session.query(User).first()
+    print(user)
     if request.method == "GET":
         return render_template("login.html")
     else:
@@ -23,30 +25,25 @@ def login():
             flash("Invalid Password", "info")
             return render_template("login.html")
         else:
-            return redirect(url_for("home"))
+            db_session.commit()
+            return render_template("profile.html", user=user)
 
-@app.route("/")
-@app.route("/profile")
-def profile():
-    return
-render_template("profile.html")
 
-@app.route("/signup")
+@app.route("/profile", methods=["Get", "POST"])
 def profile():
+    user = db_session.query(User).first()
     if "username" in session:
-        return render_template("profile.html")
-    else:
-        return redirect(url_for("login"))
+        print("user in session")
+        user = db_session.query(User).filter(User.username == session["username"]).first()
+        if user:
+            return render_template("profile.html", user=user)
+    print("user not in session")
+    return render_template("profile.html", user=user)
 
-@app.route("/login")
-def profile():
-    return
-render_template("login.html")
 
 @app.route("/matches")
-def profile():
-    return
-render_template("matches.html")
+def matches():
+    return render_template("matches.html")
 
 @app.route("/logout")
 def logout():
@@ -60,6 +57,58 @@ def logout():
 #     init_db()
     
 
+@app.route("/signup", methods = ["GET", "POST"])
+def sign_up():
+    if request.method == "POST":
+        password = request.form["password"]
+        confirm_password = request.form["confirm-password"]
+        if password != confirm_password:
+            print("Error: Passwords do not match.")
+            return False
+        user_exists = db_session.query(User).filter(User.username == request.form["username"]).first()
+        if user_exists:
+            print("Error: Username already exists.")
+            return False
+        interest_container = {
+            "sports": False,
+            "music": False,
+            "traveling": False,
+            "reading": False,
+            "art": False,
+            "dance": False,
+            "video-games": False,
+            "working-out": False,
+            "cooking": False,
+        }
+        for interest in interest_container:
+            if interest in request.form:
+                interest_container[interest] = True
+        new_user = User(
+            username=request.form["username"],
+            password=request.form["password"],
+            name=request.form["name"],
+            gender=request.form["gender"],
+            university=request.form["university"],
+            bio=request.form["bio"],
+            sports=interest_container["sports"],
+            music=interest_container["music"],
+            traveling=interest_container["traveling"],
+            reading=interest_container["reading"],
+            art=interest_container["art"],
+            dance=interest_container["dance"],
+            video_games=interest_container["video-games"],
+            working_out=interest_container["working-out"],
+            cooking=interest_container["cooking"],
+        )
+        db_session.add(new_user)
+        db_session.commit()
+
+        session["username"] = new_user.username
+        return redirect(url_for("profile"))
+    else:
+        return render_template("signup.html")
+    
 if __name__ == "__main__":
     init_db()
+    # app.run(host="172.16.7.243", port="5001")
     app.run(debug=True)
--- models.py
+++ models.py
@@ -4,9 +4,54 @@ that will be used to create objects
 and connect to data tables.
 """
 
-from sqlalchemy import ForeignKey, Column, INTEGER, TEXT
+from sqlalchemy import ForeignKey, Column, INTEGER, TEXT, String, Boolean
+import enum
 from sqlalchemy.orm import relationship
-from database import Base
+from database import Base, db_session, init_db
 
 # TODO: Complete your models
+
+class User(Base):
+    __tablename__ = 'users'
+    user_id = Column("id", INTEGER, primary_key=True)
+    username = Column("username", String(50), unique=True)
+    name = Column("name", TEXT)
+    university = Column("university", String(50))
+    password = Column("password", String(50))
+    gender = Column("gender", String(50))
+    bio = Column("bio", String(250))
+    sports = Column("sports", Boolean)
+    music = Column("music", Boolean)
+    traveling = Column("traveling", Boolean)
+    reading = Column("reading", Boolean)
+    art = Column("art", Boolean)
+    dance = Column("dance", Boolean)
+    video_games = Column("video_games", Boolean)
+    working_out = Column("working_out", Boolean)
+    cooking = Column("cooking", Boolean)
+
+
+    #relationships
+
+    def __init__(self, username, password, name, university, gender, bio, sports, music, traveling, reading, art, dance, video_games, working_out, cooking):
+        self.username=username
+        self.password=password
+        self.name=name
+        self.university=university
+        self.gender=gender
+        self.bio=bio
+        self.sports=sports
+        self.music=music
+        self.traveling=traveling
+        self.reading=reading
+        self.art=art
+        self.dance=dance
+        self.video_games=video_games
+        self.working_out=working_out
+        self.cooking=cooking
+
+        
+
+    def __repr__(self):
+        return "@" + self.username
     
